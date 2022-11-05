0cd9f82d636faac72fd27d80a7f72f27be165cd5
Shahzod Kakhkhorov <shahzod1505@gmail.com>
2022-11-06 00:30:30 +0500

    fix: delete unused component

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,12 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BoardApiService } from './services/board-api.service';
 import { Interceptor } from '../core/services/interceptor';
-import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
-  declarations: [
-    MainPageComponent,
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
