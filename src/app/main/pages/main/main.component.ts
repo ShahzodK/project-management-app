f11414ea3793d68926bd6b8d717963005793646f
Shahzod Kakhkhorov <shahzod1505@gmail.com>
2022-11-07 02:40:43 +0500

    feat: create main-page

---
+++ src/app/main/pages/main/main.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { BoardService } from './../../services/board.service';
+import { BoardApiService } from './../../services/board-api.service';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+})
+export class MainComponent implements OnInit {
+
+  constructor(public translateService: TranslateService, public router: Router, public boardService: BoardService, public api: BoardApiService) { }
+
+  ngOnInit(): void {
+    this.api.getBoards().subscribe({
+      next: (boards) => {
+        this.boardService.boards = boards;
+        this.boardService.boards.forEach(board => {
+          board.isDeleteModalVisible = false;
+        });
+      },
+      error: (err) => console.log(`oops something went wrong, status:${err}`),
+    });
+
+  }
+} 
