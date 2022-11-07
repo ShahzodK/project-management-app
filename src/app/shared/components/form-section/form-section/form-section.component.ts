1799d4f45e3cc4091d9538b4d4cc40074e39e307
Vasily Gusev <vasily.guseff@gmail.com>
2022-11-07 18:45:45 +0300

    fix: remove redundant

--- src/app/shared/components/form-section/form-section/form-section.component.ts
+++ src/app/shared/components/form-section/form-section/form-section.component.ts
@@ -1,17 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-section',
   templateUrl: './form-section.component.html',
   styleUrls: ['./form-section.component.scss'],
 })
-export class FormSectionComponent implements OnInit {
-
-  // eslint-disable-next-line @typescript-eslint/no-useless-constructor, @typescript-eslint/no-empty-function
-  constructor() { }
-
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit(): void {
-  }
-
-}
+export class FormSectionComponent { }
