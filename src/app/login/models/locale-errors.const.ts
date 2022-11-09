9dc4450afa0ce83fde2332c0244c9b3dfe5a146a
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-11-10 00:08:40 +0600

    fix: remove eng translations from ru locale file and combine different form errors translations into one

--- src/app/login/models/locale-errors.const.ts
+++ src/app/login/models/locale-errors.const.ts
@@ -1,28 +1,28 @@
 export const signUpErrorsLocale = {
   name: {
-    required: 'login-module.signup-errors.name.required',
-    minLength: 'login-module.signup-errors.name.minlength',
+    required: 'login-module.form-errors.name.required',
+    minLength: 'login-module.form-errors.name.minlength',
   },
   email: {
-    required: 'login-module.signup-errors.email.required',
-    email: 'login-module.signup-errors.email.email',
+    required: 'login-module.form-errors.email.required',
+    email: 'login-module.form-errors.email.email',
   },
   password: {
-    required: 'login-module.signup-errors.password.required',
-    enough_chars: 'login-module.signup-errors.password.enough_chars',
-    lowercase: 'login-module.signup-errors.password.lowercase',
-    uppercase: 'login-module.signup-errors.password.uppercase',
-    numeric: 'login-module.signup-errors.password.numeric',
-    specials: 'login-module.signup-errors.password.specials',
+    required: 'login-module.form-errors.password-signup.required',
+    enough_chars: 'login-module.form-errors.password-signup.enough_chars',
+    lowercase: 'login-module.form-errors.password-signup.lowercase',
+    uppercase: 'login-module.form-errors.password-signup.uppercase',
+    numeric: 'login-module.form-errors.password-signup.numeric',
+    specials: 'login-module.form-errors.password-signup.specials',
   },
 };
 
 export const signInErrorsLocale = {
   email: {
-    required: 'login-module.signin-errors.email.required',
-    email: 'login-module.signin-errors.email.email',
+    required: 'login-module.form-errors.email.required',
+    email: 'login-module.form-errors.email.email',
   },
   password: {
-    required: 'login-module.signin-errors.password.required',
+    required: 'login-module.form-errors.password-signin.required',
   },
 };
