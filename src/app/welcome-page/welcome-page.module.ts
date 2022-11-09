f5de8b62e2cc1fad699a8bc0e7de14cfd8855842
Vasily Gusev <vasily.guseff@gmail.com>
2022-11-09 09:43:25 +0300

    fix: linter errors

--- src/app/welcome-page/welcome-page.module.ts
+++ src/app/welcome-page/welcome-page.module.ts
@@ -9,7 +9,6 @@ import { TranslateLoader, TranslateModule, MissingTranslationHandler } from '@ng
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
 import { MissingTranslationService } from '../shared/services/missing-translation.service';
-import { FooterComponent } from '../core/components/footer/footer.component';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
   return new TranslateHttpLoader(http, './assets/locale/', '.json');
@@ -17,7 +16,6 @@ export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
 @NgModule({
   declarations: [
     WelcomePageComponent,
-    FooterComponent,
   ],
   imports: [
     CommonModule,
