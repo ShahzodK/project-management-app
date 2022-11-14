567906ba037aa0c5289660c2825ebf661a91b3d6
Shahzod Kakhkhorov <shahzod1505@gmail.com>
2022-11-15 02:35:33 +0500

    fix: change url in environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: true,
   locales: Object.keys(ELocales),
   defaultLocale: ELocales.EN,
-  baseUrl: 'https://shocking-monster-70192.herokuapp.com/',
+  baseUrl: 'https://fathomless-everglades-64985.herokuapp.com/',
 };
