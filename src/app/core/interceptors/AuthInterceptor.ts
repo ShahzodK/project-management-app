c0e23d0ecc9842fa7806e29d558aa6b24fbc66e7
Shahzod Kakhkhorov <shahzod1505@gmail.com>
2022-11-21 02:07:39 +0500

    feat: redirect to welcome page if token expires(broken)

--- src/app/core/interceptors/AuthInterceptor.ts
+++ src/app/core/interceptors/AuthInterceptor.ts
@@ -1,20 +1,28 @@
+import { resetUser } from 'src/app/redux/actions';
+import { catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { environment } from '../../../environments/environment';
+import { Store } from '@ngrx/store';
 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router, private store: Store) {}
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authToken = localStorage.getItem('authToken');
     if (request.url.includes('assets')) {
       return next.handle(request);
     }
 
     const url = `${environment.baseUrl}${request.url}`;
     const headers = request.headers
-      .append('Authorization', `Bearer ${localStorage.getItem('authToken')}`)
+      .append('Authorization', `Bearer ${authToken}`)
       .append('Accept', 'application/json');
 
     const customReq = request.clone({
@@ -22,6 +30,18 @@ export class AuthInterceptor implements HttpInterceptor {
       headers,
     });
 
-    return next.handle(customReq);
+    return next.handle(customReq).pipe(catchError((err) => {
+      if (err instanceof HttpErrorResponse || !request.url.includes('auth/login') || !request.url.includes('auth/signup')) {
+        if (err.status !== 401) {
+          throw Error(err);
+        }
+        this.store.dispatch(resetUser());
+        localStorage.removeItem('authToken');
+        this.router.navigateByUrl('/welcome');
+      }
+      throw Error(err);
+    }));
+
+
   }
 }
