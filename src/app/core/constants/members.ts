a9214390c3735ba0ceab330b7623ec34784aded0
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-11-24 23:02:45 +0600

    refactor: move members array to separate file and change class names of footer

---
+++ src/app/core/constants/members.ts
@@ -0,0 +1,27 @@
+export const members = [
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/4479289?v=4',
+    name: 'core.members.first.name',
+    description: 'welcome.members.first.desc',
+    github: 'https://github.com/Guseff'
+  },
+  {
+
+    avatar: 'https://avatars.githubusercontent.com/u/66957732?s=400&u=54841e02f487bfb3deac9cc8274d1b1ac16e7c40&v=4',
+    name: 'core.members.second.name',
+    description: 'core.members.second.desc',
+    github: 'https://github.com/adiletmaksatuly'
+  },
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/57046438?v=4',
+    name: 'core.members.third.name',
+    description: 'core.members.third.desc',
+    github: 'https://github.com/ShahzodK'
+  },
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/56885229?v=4',
+    name: 'core.members.fourth.name',
+    description: 'core.members.fourth.desc',
+    github: 'https://github.com/dsh91698'
+  },
+];
--- src/app/welcome/pages/welcome-page/welcome-page.component.ts
+++ src/app/welcome/pages/welcome-page/welcome-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { members } from 'src/app/core/constants/members';
 
 
 @Component({
@@ -9,30 +10,7 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./welcome-page.component.scss'],
 })
 export class WelcomePageComponent {
-  public members = [
-    {
-      avatar: 'https://avatars.githubusercontent.com/u/4479289?v=4',
-      name: 'core.members.first.name',
-      description: 'welcome.members.first.desc',
-    },
-    {
-
-      avatar: 'https://avatars.githubusercontent.com/u/66957732?s=400&u=54841e02f487bfb3deac9cc8274d1b1ac16e7c40&v=4',
-      name: 'core.members.second.name',
-      description: 'core.members.second.desc',
-    },
-    {
-      avatar: 'https://avatars.githubusercontent.com/u/57046438?v=4',
-      name: 'core.members.third.name',
-      description: 'core.members.third.desc',
-    },
-    {
-      avatar: 'https://avatars.githubusercontent.com/u/56885229?v=4',
-      name: 'core.members.fourth.name',
-      description: 'core.members.fourth.desc',
-    },
-  ];
+  public members = members;
 
   constructor(public translateService: TranslateService, public router: Router) {}
-
 }
