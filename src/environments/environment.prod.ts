b6781e2fcf73c23b4ebba864fa9ec65e3e9b400a
Shahzod Kakhkhorov <57046438+ShahzodK@users.noreply.github.com>
2023-01-24 17:37:13 +0500

    fix backend link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: true,
   locales: Object.keys(ELocales),
   defaultLocale: ELocales.EN,
-  baseUrl: 'https://teamwork.herokuapp.com/,
+  baseUrl: 'https://teamwork.herokuapp.com/',
 };
